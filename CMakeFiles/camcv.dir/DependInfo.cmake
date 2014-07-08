6cb23f51a4f20543171e8695d5612c51907551e4
Doug McInnes <doug@dougmcinnes.com>
2014-07-07 20:53:21 -0700

    initial commit

---
+++ CMakeFiles/camcv.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/camcv/RaspiCLI.c" "/home/pi/camcv/CMakeFiles/camcv.dir/RaspiCLI.c.o"
+  "/home/pi/camcv/RaspiCamControl.c" "/home/pi/camcv/CMakeFiles/camcv.dir/RaspiCamControl.c.o"
+  "/home/pi/camcv/RaspiPreview.c" "/home/pi/camcv/CMakeFiles/camcv.dir/RaspiPreview.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/camcv/camcv_vid1.cpp" "/home/pi/camcv/CMakeFiles/camcv.dir/camcv_vid1.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "/opt/vc/userland/host_applications/linux/libs/bcm_host/include"
+  "/opt/vc/userland/interface/vcos"
+  "/opt/vc/userland"
+  "/opt/vc/userland/interface/vcos/pthreads"
+  "/opt/vc/userland/interface/vmcs_host/linux"
+  "/opt/vc/userland/interface/khronos/include"
+  "/opt/vc/userland/interface/khronos/common"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/camcv.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/camcv.dir/RaspiCamControl.c.o"
+  "CMakeFiles/camcv.dir/RaspiCLI.c.o"
+  "CMakeFiles/camcv.dir/RaspiPreview.c.o"
+  "CMakeFiles/camcv.dir/camcv_vid1.cpp.o"
+  "camcv.pdb"
+  "camcv"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/camcv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
